385bed72c97f097b3bb02fd06f6e26d2c32dddac
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-09-11 12:14:52 +0100

    Update 99_Trick.py

--- Challenge 3/99_Trick.py
+++ Challenge 3/99_Trick.py
@@ -5,13 +5,15 @@ def minusnine(x):
 def result(i,j):
     return j - (int(str(i + j)[1::]) + int(str(i + j)[0]))
 
-ans = int(input('First Player:\nEnter a number between 10 and 49: '))
-while ans < 10 or ans > 49:
-  ans = int(input('Enter a number between 10 and 49: '))
-factor = minusnine (ans)
+if __name__ == '__main__':
+    ans = int(input('First Player:\nEnter a number between 10 and 49: '))
+    while ans < 10 or ans > 49:
+        ans = int(input('Enter a number between 10 and 49: '))
+    factor = minusnine (ans)
 
-frans = int(input('\nSecond Player:\nEnter a number between 50 and 99: '))
-while frans < 50 or frans > 99:
-  frans = int(input('Enter a number between 50 and 99: '))
+    frans = int(input('\nSecond Player:\nEnter a number between 50 and 99: '))
+    while frans < 50 or frans > 99:
+        frans = int(input('Enter a number between 50 and 99: '))
   
-print(f'\nI said the answer was {ans} and the calculation result is {result(factor,frans)}')
+    print(f'\nI said the answer was {ans} and the calculation result is {result(factor,frans)}')
+
