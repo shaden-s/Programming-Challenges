05ed91f76c8fa90cbadcca947a09aafae8ae10f7
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-09-11 12:33:51 +0100

    Update wind_chill.py

--- Challenge 4/wind_chill.py
+++ Challenge 4/wind_chill.py
@@ -1,16 +1,15 @@
 #CHALLENGE 4
-
 def calculatewindchill(temp, speed):
   windchill = 35.74 + (0.6215 * temp) - (35.75 * speed**0.16) + (0.4275*temp*(speed**0.16))
   return windchill
-
+dash = '_' * 25
 def present(temp, speed):
-  print(f'_______________________________________________\nTemperature: {temp}\nSpeed: {speed}\nWindchill: {calculatewindchill(temp, speed)}\n_______________________________________________')
+  print(f'{dash}\nTemperature: {temp}\nSpeed: {speed}\nWindchill: {calculatewindchill(temp, speed)}\n{dash}')
 
-present(10, 15)
-present(0, 25)
-present(-10, 35)
-utemp = float(input('Enter Temperature: '))
-uspeed = float(input('Enter Speed: '))
-present(utemp, uspeed)
-  
+if __name__ == '__main__':
+    present(10, 15)
+    present(0, 25)
+    present(-10, 35)
+    utemp = float(input('Enter Temperature: '))
+    uspeed = float(input('Enter Speed: '))
+    present(utemp, uspeed)
