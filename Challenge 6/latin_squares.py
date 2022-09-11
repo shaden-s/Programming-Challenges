3b75fe789cac95ffa8d47bbb7f907d63659b26e1
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-09-11 18:03:38 +0100

    Update latin_squares.py

--- Challenge 6/latin_squares.py
+++ Challenge 6/latin_squares.py
@@ -1,29 +1,34 @@
 #CHALLENGE 6
-order = int(input('Input the order of the square: '))
-topleft = int(input('Input the top left of the sqaure: '))
+def generatelatin(order, c):
+  lines = []
+  for i in range(order):
+    for x in range(order + 1):
+      if c <= order:
+        lines.append(str(c))
+        c += 1
+      else:
+        c = 1
+    c += 1
+  return lines
 
-while topleft >= order:
-  print('The top left number must be between 1 and the first number entered.')
+def drawlatin(arr, order):
+  d=0
+  str = ''
+  for x in range(order):
+    for i in range(order):
+      str += arr[d] + ' '
+      d+=1
+    str += '\n'
+  return str
+    
+if __name__ == '__main__':
+  order = int(input('Input the order of the square: '))
   topleft = int(input('Input the top left of the sqaure: '))
 
-lines = []
-c = topleft
-for x in range(order):
-  for x in range(order + 1):
-    if c <= order:
-      lines.append(str(c))
-      c += 1
-    else:
-      c = 1
-  c += 1
+  while topleft >= order:
+    print('The top left number must be between 1 and the first number entered.')
+    topleft = int(input('Input the top left of the sqaure: '))
 
+  latin = generatelatin(order, topleft)
 
-print('This is your Latin Square:\n')
-d=0
-for x in range(order):
-  str=''
-  for i in range(order):
-    str += lines[d] + ' '
-    d+=1
-  print(str)
-    
+  print(f'This is your Latin Square:\n{drawlatin(latin, order)}')
