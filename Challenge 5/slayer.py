eb4c56843da3ba3367a1f8bb95d332cef5ddf11d
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-09-11 12:42:02 +0100

    Update slayer.py

--- Challenge 5/slayer.py
+++ Challenge 5/slayer.py
@@ -1,20 +1,24 @@
 #CHALLENGE 5 
+def layers(x):
+    stringg = str(x)
+    return stringg[1::] + stringg[0]
 
-print('Guess a six-digit number SLAYER so that following equation is true, where each letter stands for the diGit in th position shows: SLAYER + SLAYER + SLAYER = LAYERS.')
+def check(z,y):
+    if int(goal) == int(layers(guess)):
+        print('Your guess is correct.')
+    else:
+        print('Your guess in incorrect')
+        print('Thanks for playing')
+        
+if __name__ == '__main__':
+    print('Guess a six-digit number SLAYER so that following equation is true, where each letter stands for the diGit in th position shows: SLAYER + SLAYER + SLAYER = LAYERS.')
 
-guess = int(input('Enter your guess for SLAYER: '))
-while len(str(guess)) != 6:
-  guess = int(input('Enter your guess for SLAYER: '))
-goal = guess * 3
+    guess = int(input('Enter your guess for SLAYER: '))
+    while len(str(guess)) != 6:
+        guess = int(input('Enter your guess for SLAYER: '))
+    goal = guess * 3
 
-stringg = str(guess)
-layers = stringg[1::] + stringg[0]
-
-print(f'SLAYER + SLAYER + SLAYER = {goal}')
-print(f'LAYERS = {layers}')
-
-if int(goal) == int(layers):
-  print('Your guess is correct.')
-else:
-  print('Your guess in incorrect')
-print('Thanks for playing')
+    print(f'SLAYER + SLAYER + SLAYER = {goal}')
+    print(f'LAYERS = {layers(guess)}')
+    
+    check(int(goal), int(layers(guess)))
