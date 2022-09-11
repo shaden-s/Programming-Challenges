a1d9fd607b5c249fa0dec73a68686c07e5cfcb10
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-09-11 11:26:10 +0100

    Update richter.py

--- Challenge 2/richter.py
+++ Challenge 2/richter.py
@@ -1,6 +1,4 @@
 #CHALLENGE 2
-richter = float(input('Enter Richter scale value: '))
-
 def energy(i):
   return 10**((1.5*i) + 4.8)
 def tnt(k):
@@ -8,13 +6,18 @@ def tnt(k):
 
 def present(r):
   print(f'\n{r:<12}{energy(r):<30}{tnt(r):<30}\n')
+  
+if __name__ == '__main__':
+    richter = float(input('Enter Richter scale value: '))
+    
+    print('\nRichter     Joules                        TNT')
+    present(1)
+    present(5)
+    present(9.1)
+    present(9.2)
+    present(9.5)
 
-print('\nRichter     Joules                        TNT')
-present(1)
-present(5)
-present(9.1)
-present(9.2)
-present(9.5)
+
+    print(f'Richter Value: {richter}\nEquivalence in Joules: {energy(richter)} \nEquivalence in tons of TNT: {tnt(richter)}')
 
 
-print(f'Richter Value: {richter}\nEquivalence in Joules: {energy(richter)} \nEquivalence in tons of TNT: {tnt(richter)}')
