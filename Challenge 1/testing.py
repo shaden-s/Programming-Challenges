28eedb24859e4311fabcd9d4d59e63626c8a9e1e
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-09-20 12:18:04 +0100

    Update testing.py

--- Challenge 1/testing.py
+++ Challenge 1/testing.py
@@ -5,11 +5,16 @@ from rod_conversion import *
 
 class MyFirstTests(unittest.TestCase):
 
-    def test_metres(self):
-        self.assertEqual(metres(1), 5.0292)
-
-    def test_furlong(self):
-        self.assertEqual(furlong(10), 0.25)
+      def test_joules(self):
+        self.assertIsNotNone(joules(1), "This should return a value")
+        self.assertEqual(joules(1), 1995262.3149688789)
+        self.assertEqual(joules(3.4251), 8662634684.989311)
+        
+    def test_tnt(self):
+        self.assertIsNotNone(tnt(1), "This should return a value")
+        self.assertEqual(tnt(10), 15080242351.8)
+        self.assertEqual(tnt(1), 0.00047687913)
+        self.assertEqual(tnt(3.4251), 2.07041937978)
 
 if __name__ == '__main__':
     unittest.main()
