cb0e13ace12d32b9959b46e8c69bccb9a31f1782
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-10-30 20:14:15 +0000

    Update testing.py

--- Challenge 2/testing.py
+++ Challenge 2/testing.py
@@ -7,9 +7,13 @@ class MyFirstTests(unittest.TestCase):
 
     def test_energy(self):
         self.assertEqual(energy(1), 1995262.3149688789)
+        self.assertIsNotNone(energy(5), "This should return a value")
+        self.assertEqual(energy(5), 1995262314968.8828)
 
     def test_tnt(self):
         self.assertEqual(tnt(10), 15080242458.895658)
+        self.assertIsNotNone(energy(5), "This should return a value")
+        self.assertEqual(energy(5), 476.87913837688404)
 
 if __name__ == '__main__':
     unittest.main()
