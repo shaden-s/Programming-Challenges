83789058dcab09e66110d1adffd336e8cfedab33
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-10-30 20:10:18 +0000

    Update o196.py

--- Challenge 7/o196.py
+++ Challenge 7/o196.py
@@ -17,24 +17,29 @@ def checklych(y):
       c += 1
       y=revadd(y)
   return False
-  
+
+def mainloop(ione, itwo):
+	palcount = 0
+	lychrel = []
+	nonlych = 0
+	for i in range(ione, itwo+1):
+		if palindrome(i):
+			palcount += 1
+		else:
+			if checklych(revadd(i)):
+				nonlych += 1
+			else:
+				lychrel.append(i)
+    
+	printer(lychrel, palcount, nonlych)
+
+def printer(lych,pal,non):
+	for x in lych:
+		print(f'{x} is probably lychrel')
+		print(f'Palindrome numbers: {pal}\nNot Lychrel numbers: {non}\nLychrel = {len(lych)}')
       
 if __name__ == '__main__':
   start = int(input('Integer 1: '))
   end = int(input('Integer 2: '))
   
-  palcount = 0
-  lychrel = []
-  nonlych = 0
-  for i in range(start, end+1):
-    if palindrome(i):
-      palcount += 1
-    else:
-      if checklych(revadd(i)):
-        nonlych += 1
-      else:
-        lychrel.append(i)
-      
-  for x in lychrel:
-    print(f'{x} is probably lychrel')
-  print(f'Palindrome numbers: {palcount}\nNot Lychrel numbers: {nonlych}\nLychrel = {len(lychrel)}')
+  mainloop(start,end)
